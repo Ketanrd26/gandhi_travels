af5cf9538cfa68ef38efcb18e7d71a90ffc3fb26
ketanrd26 <ketandudka566123@gmail.com>
2024-11-14 18:17:55 +0530

    push

---
+++ src/comp/pagetop/Pagetop.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./pagetop.scss"
+const Pagetop = () => {
+  return (
+    <>
+      <div class="pagetop-parent bg-img-cover parent">
+        <div class="page-top-cont cont">
+            <h1>
+                Vehicels
+            </h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Pagetop
