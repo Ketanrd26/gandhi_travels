0c150fc5fed6697c548c0bf977027194b178ebd0
ketanrd26 <ketandudka566123@gmail.com>
2024-11-14 16:58:38 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Header from "./comp/header/Header";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
--- src/comp/header/Header.js
+++ src/comp/header/Header.js
@@ -1,11 +1,85 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import "./header.scss";
+import { Link } from "react-router-dom";
+import logo from "../../assests/logo.png";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross1 } from "react-icons/rx";
 const Header = () => {
+  const NavLinks = [
+    {
+      link_name: "Home",
+      link_path: "/",
+    },
+    {
+      link_name: "About",
+      link_path: "/about",
+    },
+    {
+      link_name: "Vehicels",
+      link_path: "/vehicels",
+    },
+    {
+      link_name: "Blogs",
+      link_path: "/blogs",
+    },
+  ];
+
+  const [scrolled, setScrolled] = useState(false);
+
+  if (window.scrollY > 80) {
+    setScrolled(true);
+  }
+
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
   return (
     <>
-      
+      <div
+        class={
+          scrolled ? "header-parent active parent" : "header-parent parent"
+        }
+      >
+        <div class="header-cont cont">
+          <div class="logo">
+            <img src={logo} alt="" />
+          </div>
+          <div class="nav-list">
+            {NavLinks.map((item, index) => (
+              <Link to={item.link_path} className="nav" key={index}>
+                {item.link_name}
+              </Link>
+            ))}
+            <Link to="/contact" className="btn">
+              Contact us
+            </Link>
+          </div>
+          <div
+            class="hamburger"
+            onClick={() => setMobileNavOpen(!mobileNavOpen)}
+          >
+            {!mobileNavOpen ? <RxHamburgerMenu /> : <RxCross1 />}
+          </div>
+        </div>
+      </div>
+   {mobileNavOpen &&
+       <div class="mobile-nav parent">
+       <div class="overlay-nav parent">
+         <div class="mobile-nav-cont cont">
+          {
+           NavLinks.map((item,index)=>(
+               <Link to={item.link_path} className="link" key={index} >
+         {  item.link_name}
+               </Link>
+           ))
+          }
+           <Link to="/contact" className="btn">
+             Contact us
+           </Link>
+         </div>
+       </div>
+     </div>
+   }
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
