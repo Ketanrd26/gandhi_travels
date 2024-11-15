ae3ed68e3ee556b6ccc1711cee7d1f7b7917d8a4
ketanrd26 <ketandudka566123@gmail.com>
2024-11-15 15:52:11 +0530

    push

--- src/comp/pagetop/Pagetop.js
+++ src/comp/pagetop/Pagetop.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import "./pagetop.scss"
-const Pagetop = () => {
+const Pagetop = ({pagetop_text}) => {
   return (
     <>
       <div class="pagetop-parent bg-img-cover parent">
         <div class="page-top-cont cont">
             <h1 className='heading'>
-                Vehicels
+                {pagetop_text}
             </h1>
         </div>
       </div>
