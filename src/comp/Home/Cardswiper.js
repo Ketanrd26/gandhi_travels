eb08b9e025a281a69f8d8c34e80191ef2afef5dc
Sunil Shelke <shelkesunil072@gmail.com>
2024-11-18 15:41:25 +0530

    ok responsive

--- src/comp/Home/Cardswiper.js
+++ src/comp/Home/Cardswiper.js
@@ -62,6 +62,20 @@ const Cardswiper = () => {
         pagination={{
           clickable: true,
         }}
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+        }}
   
         className="mySwiper"
       >
