3d283cb6e3f079a3b0c8ae38c5a17930b211ac25
Sunil Shelke <shelkesunil072@gmail.com>
2024-11-21 12:34:58 +0530

    NEW
    NEW

--- src/comp/Home/Testimonial.js
+++ src/comp/Home/Testimonial.js
@@ -1,6 +1,5 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css/autoplay";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation, Autoplay } from "swiper/modules";
@@ -8,69 +7,65 @@ import "./testimonial.scss";
 
 const Testimonial = () => {
   const swiperRef = useRef(null);
+  const [lightboxOpen, setLightboxOpen] = useState(false);
+  const [lightboxImage, setLightboxImage] = useState("");
 
-  // Sample image data array (replace with your actual data)
   const images = [
     {
       id: 1,
-      leftTop: "https://as1.ftcdn.net/v2/jpg/03/89/37/68/1000_F_389376832_DzMVS7vo8kI7oKRA0NTfvfNRBCrlCa2G.jpg",
-      leftBottom: "https://t3.ftcdn.net/jpg/02/98/35/82/240_F_298358259_bwYxOvtrqJn7m8dfeYkkoNkusBSYNhep.jpg",
-      middleTop: "https://t3.ftcdn.net/jpg/02/98/35/82/240_F_298358259_bwYxOvtrqJn7m8dfeYkkoNkusBSYNhep.jpg",
-      middleBottom: "https://as1.ftcdn.net/v2/jpg/03/89/37/68/1000_F_389376832_DzMVS7vo8kI7oKRA0NTfvfNRBCrlCa2G.jpg",
-      rightTop: "https://as1.ftcdn.net/v2/jpg/03/89/37/68/1000_F_389376832_DzMVS7vo8kI7oKRA0NTfvfNRBCrlCa2G.jpg",
-      rightBottom: "https://t3.ftcdn.net/jpg/02/98/35/82/240_F_298358259_bwYxOvtrqJn7m8dfeYkkoNkusBSYNhep.jpg",
-    },
-    {
-      id: 2,
-      leftTop: "https://t3.ftcdn.net/jpg/02/98/35/82/240_F_298358259_bwYxOvtrqJn7m8dfeYkkoNkusBSYNhep.jpg",
-      leftBottom: "https://t3.ftcdn.net/jpg/02/98/35/82/240_F_298358259_bwYxOvtrqJn7m8dfeYkkoNkusBSYNhep.jpg",
-      middleTop: "https://t3.ftcdn.net/jpg/02/98/35/82/240_F_298358259_bwYxOvtrqJn7m8dfeYkkoNkusBSYNhep.jpg",
-      middleBottom: "https://t3.ftcdn.net/jpg/02/98/35/82/240_F_298358259_bwYxOvtrqJn7m8dfeYkkoNkusBSYNhep.jpg",
-      rightTop: "https://t3.ftcdn.net/jpg/02/98/35/82/240_F_298358259_bwYxOvtrqJn7m8dfeYkkoNkusBSYNhep.jpg",
-      rightBottom: "https://t3.ftcdn.net/jpg/02/98/35/82/240_F_298358259_bwYxOvtrqJn7m8dfeYkkoNkusBSYNhep.jpg",
-    },
-    {
-      id: 3,
-      leftTop: "https://t4.ftcdn.net/jpg/01/29/68/29/240_F_129682911_QPLvhcTpuxXR1Ih5HG5U56ilELjAQH2g.jpg",
-      leftBottom: "https://t4.ftcdn.net/jpg/01/29/68/29/240_F_129682911_QPLvhcTpuxXR1Ih5HG5U56ilELjAQH2g.jpg",
-      middleTop: "https://t4.ftcdn.net/jpg/01/29/68/29/240_F_129682911_QPLvhcTpuxXR1Ih5HG5U56ilELjAQH2g.jpg",
-      middleBottom: "https://t4.ftcdn.net/jpg/01/29/68/29/240_F_129682911_QPLvhcTpuxXR1Ih5HG5U56ilELjAQH2g.jpg",
-      rightTop: "https://t4.ftcdn.net/jpg/01/29/68/29/240_F_129682911_QPLvhcTpuxXR1Ih5HG5U56ilELjAQH2g.jpg",
-      rightBottom: "https://t4.ftcdn.net/jpg/01/29/68/29/240_F_129682911_QPLvhcTpuxXR1Ih5HG5U56ilELjAQH2g.jpg",
+      leftTop:
+        "https://as1.ftcdn.net/v2/jpg/03/89/37/68/1000_F_389376832_DzMVS7vo8kI7oKRA0NTfvfNRBCrlCa2G.jpg",
+      leftBottom:
+        "https://t3.ftcdn.net/jpg/02/98/35/82/240_F_298358259_bwYxOvtrqJn7m8dfeYkkoNkusBSYNhep.jpg",
+      middleTop:
+        "https://t3.ftcdn.net/jpg/02/98/35/82/240_F_298358259_bwYxOvtrqJn7m8dfeYkkoNkusBSYNhep.jpg",
+      middleBottom:
+        "https://as1.ftcdn.net/v2/jpg/03/89/37/68/1000_F_389376832_DzMVS7vo8kI7oKRA0NTfvfNRBCrlCa2G.jpg",
+      rightTop:
+        "https://as1.ftcdn.net/v2/jpg/03/89/37/68/1000_F_389376832_DzMVS7vo8kI7oKRA0NTfvfNRBCrlCa2G.jpg",
+      rightBottom:
+        "https://t3.ftcdn.net/jpg/02/98/35/82/240_F_298358259_bwYxOvtrqJn7m8dfeYkkoNkusBSYNhep.jpg",
     },
   ];
 
-  // Handle mouse enter to stop autoplay
   const handleMouseEnter = () => {
     if (swiperRef.current) {
       swiperRef.current.swiper.autoplay.stop();
     }
   };
 
-  // Handle mouse leave to start autoplay
   const handleMouseLeave = () => {
     if (swiperRef.current) {
       swiperRef.current.swiper.autoplay.start();
     }
   };
 
+  const openLightbox = (imageUrl) => {
+    setLightboxImage(imageUrl);
+    setLightboxOpen(true);
+  };
+
+  const closeLightbox = () => {
+    setLightboxOpen(false);
+    setLightboxImage("");
+  };
+
   return (
     <div
       className="testimonial-parent parent"
-      onMouseEnter={handleMouseEnter} // Add mouse enter to stop autoplay
-      onMouseLeave={handleMouseLeave} // Add mouse leave to start autoplay
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <div className="testimonial-cont cont">
         <Swiper
-          ref={swiperRef} // Reference for controlling autoplay
-          navigation={true}
-          loop={true} 
-          modules={[Navigation, Autoplay]}
+          ref={swiperRef}
+          navigation={true} // Enable navigation
+          loop={true}
+          modules={[Navigation, Autoplay]} // Ensure Navigation module is included
           autoplay={{
-            delay: 2500, // Delay between slides in milliseconds
-            disableOnInteraction: false, // Prevent disabling autoplay after user interaction
+            delay: 2500,
+            disableOnInteraction: false,
           }}
-          
           className="mySwiper"
         >
           {images.map((image) => (
@@ -80,30 +75,36 @@ const Testimonial = () => {
                   <div
                     className="top bg-img-cover"
                     style={{ backgroundImage: `url(${image.leftTop})` }}
+                    onClick={() => openLightbox(image.leftTop)}
                   ></div>
                   <div
                     className="bottom bg-img-cover"
                     style={{ backgroundImage: `url(${image.leftBottom})` }}
+                    onClick={() => openLightbox(image.leftBottom)}
                   ></div>
                 </div>
                 <div className="middle-card">
                   <div
                     className="top bg-img-cover"
                     style={{ backgroundImage: `url(${image.middleTop})` }}
+                    onClick={() => openLightbox(image.middleTop)}
                   ></div>
                   <div
                     className="bottom bg-img-cover"
                     style={{ backgroundImage: `url(${image.middleBottom})` }}
+                    onClick={() => openLightbox(image.middleBottom)}
                   ></div>
                 </div>
                 <div className="right-card">
                   <div
                     className="top bg-img-cover"
                     style={{ backgroundImage: `url(${image.rightTop})` }}
+                    onClick={() => openLightbox(image.rightTop)}
                   ></div>
                   <div
                     className="bottom bg-img-cover"
                     style={{ backgroundImage: `url(${image.rightBottom})` }}
+                    onClick={() => openLightbox(image.rightBottom)}
                   ></div>
                 </div>
               </div>
@@ -111,6 +112,16 @@ const Testimonial = () => {
           ))}
         </Swiper>
       </div>
+      {lightboxOpen && (
+        <div className="lightbox" onClick={closeLightbox}>
+          <div className="lightbox-content">
+            <img src={lightboxImage} alt="Preview" />
+            <span className="close-button" onClick={closeLightbox}>
+              &times;
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
