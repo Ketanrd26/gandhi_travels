6750ea3ba52122d1e6b385d0e13ba6a7822772f0
Ketan Dudka <ketandudka566123@gmail.com>
2024-11-27 11:33:44 +0530

    new

--- src/comp/Home/Gallerycard.js
+++ src/comp/Home/Gallerycard.js
@@ -1,15 +1,63 @@
-import React from 'react'
-import "./gallerycard.scss"
+import React from 'react';
+import './gallerycard.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import { EffectCoverflow, Pagination, Autoplay } from 'swiper/modules';
+
 const Gallerycard = () => {
+  // Array of image URLs
+  const images = [
+    'https://swiperjs.com/demos/images/nature-1.jpg',
+    'https://swiperjs.com/demos/images/nature-2.jpg',
+    'https://swiperjs.com/demos/images/nature-3.jpg',
+    'https://swiperjs.com/demos/images/nature-4.jpg',
+    'https://swiperjs.com/demos/images/nature-5.jpg',
+    'https://swiperjs.com/demos/images/nature-6.jpg',
+    'https://swiperjs.com/demos/images/nature-7.jpg',
+    'https://swiperjs.com/demos/images/nature-8.jpg',
+    'https://swiperjs.com/demos/images/nature-9.jpg',
+  ];
+
   return (
-    <div class="gallerycard-parent parent">
-        <div class="gallerycard-cont cont">
-            <h2>Our Gallery</h2>
-            
-        </div>
+    <div className="gallerycard-parent parent">
+      <div className="gallerycard-cont cont">
+        <h2>Our Gallery</h2>
+        <Swiper
+          effect="coverflow"
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView="auto"
+          initialSlide={1} // Start with the second slide
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true, // Make pagination bullets clickable
+          }}
+          modules={[EffectCoverflow, Pagination, Autoplay]} // Include Autoplay module
+          className="mySwiper"
+        >
+          {images.map((image, index) => (
+            <SwiperSlide key={index}>
+              <img src={image} alt={`Slide ${index + 1}`} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Gallerycard
-
+export default Gallerycard;
