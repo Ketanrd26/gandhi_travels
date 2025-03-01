79797ea1225774f251573ffb812e7695662edee8
Sunil Shelke <shelkesunil072@gmail.com>
2025-03-01 18:22:27 +0530

    new

--- src/comp/Home/Hero.js
+++ src/comp/Home/Hero.js
@@ -16,11 +16,20 @@ import AOS from "aos"; // Import AOS library
 import "./hero.scss";
 
 // Import images
-import img1 from "../../assests/vehicles/17_s_pushback/17seater.webp";
-import img2 from "../../assests/vehicles/20_s_pushback/20_s_pushback.jpg";
-import img3 from "../../assests/vehicles/26_s_modifeid_pushback/26seat.webp";
-import img4 from "../../assests/vehicles/35_ac_bhartbanz/35_ac_bharatbenz.jpg";
-import img5 from "../../assests/vehicles/innova_crysta/innova_cryst.jpg";
+import img1 from "../../assests/vehicles/vovlo/image_5.jpg";
+import img2 from "../../assests/36seat.png";
+import img3 from "../../assests/vehicles/35_ac_bhartbanz/35_ac_bharatbenz.jpg";
+import img4 from "../../assests/12seat.png";
+import img5 from "../../assests/vehicles/20_s_pushback/20_s_pushback.jpg";
+
+
+
+
+
+
+
+
+
 import img6 from "../../assests/vehicles/kia_carnes/kia.jpg";
 import img7 from "../../assests/vehicles/luxary_urbania/urbania.webp";
 import img8 from "../../assests/vehicles/vovlo/image_5.jpg";
@@ -55,14 +64,15 @@ const Hero = () => {
   }, []);
 
   const slides = [
-    { id: 2, image: img2, mob_image: mob_img1, overlay: true },
+   
     { id: 1, image: img1, mob_image: mob_img2, overlay: true },
+    { id: 2, image: img2, mob_image: mob_img1, overlay: true },
     { id: 3, image: img3, mob_image: mob_img3, overlay: true },
     { id: 4, image: img4, mob_image: mob_img4, overlay: true },
     { id: 5, image: img5, mob_image: mob_img5, overlay: true },
-    { id: 6, image: img6, mob_image: mob_img6, overlay: true },
-    { id: 7, image: img7, mob_image: mob_img7, overlay: true },
-    { id: 8, image: img8, mob_image: mob_img8, overlay: true },
+    // { id: 6, image: img6, mob_image: mob_img6, overlay: true },
+    // { id: 7, image: img7, mob_image: mob_img7, overlay: true },
+    // { id: 8, image: img8, mob_image: mob_img8, overlay: true },
   ];
 
   return (
